cf2840ad80b90d5810953a7980353d80549d2d53
DiegoDev2 <DiegoDev2@users.noreply.github.com>
2025-02-09 01:16:59 -0400

    add Vercel Analytics for enhanced tracking and insights ✨

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins} from "next/font/google";
-
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const poppins = Poppins({
@@ -38,6 +38,7 @@ export default function RootLayout({
         className={`${poppins.className} antialiased ` }
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
